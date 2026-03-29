effd25cac29757d6d1f49b0d44866a2f36b571c0
vermaldonado26 <vermaldonado@me.com>
2026-03-29 13:49:07 -0300

    primer proyecto

---
+++ script.py
@@ -0,0 +1,15 @@
+<!--
+ls -a: lista todos los archivos inclusive los ocultos
+git init: inicializa repositorio crea .git
+git status: ve el estado del repositorio
+git add . // git add index.html: Pasa el archivo de git init a staging
+git commit -m "mensaje": pasa el archivo al repositorio local para su seguimiento,
+version guardada.
+git log --oneline: muestra todos los commits realizados ejm: 7f047b8 Comienzo del proyecto
+git reset --hard  7f047b8: mueve al head, restaura archivos, descarta cambios
+open index.html: abre el archivo directamente en el navegador
+git log: muestra información detallada
+git log --oneline --graph --decorate: 
+-->
+
+print("Hola mundo desde Python")
