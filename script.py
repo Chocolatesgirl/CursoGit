48864b6fea18ed3c1ff7b55da175393f2d336ace
vermaldonado26 <vermaldonado@me.com>
2026-03-30 11:12:01 -0300

    Este es el tercer cambio

--- script.py
+++ script.py
@@ -1,15 +1,18 @@
-<!--
+"""
+El flujo es: 1., 2., 3., 4., 5.
 ls -a: lista todos los archivos inclusive los ocultos
-git init: inicializa repositorio crea .git
+1. git init: inicializa repositorio crea .git
 git status: ve el estado del repositorio
-git add . // git add index.html: Pasa el archivo de git init a staging
-git commit -m "mensaje": pasa el archivo al repositorio local para su seguimiento,
+2. git add . // git add index.html: Pasa el archivo de git init a staging
+3. git commit -m "mensaje": pasa el archivo al repositorio local para su seguimiento,
 version guardada.
-git log --oneline: muestra todos los commits realizados ejm: 7f047b8 Comienzo del proyecto
-git reset --hard  7f047b8: mueve al head, restaura archivos, descarta cambios
+4. git log --oneline: muestra todos los commits realizados ejm: 7f047b8 Comienzo del proyecto
+5. git reset hard f047b8: mueve al head, restaura archivos, descarta cambios
 open index.html: abre el archivo directamente en el navegador
 git log: muestra información detallada
 git log --oneline --graph --decorate: 
--->
+"""
 
 print("Hola mundo desde Python")
+print("Segunda linea")
+print("Tercera linea")
