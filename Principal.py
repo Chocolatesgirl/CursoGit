094984b8280f937c42feda12a396106c281f2f10
vermaldonado26 <vermaldonado@me.com>
2026-03-30 13:35:44 -0300

    Mi primer proyecto sola

---
+++ Principal.py
@@ -0,0 +1,15 @@
+from Proyecto1 import *
+
+print("++++++++++ Esto es una calculadora++++++++++")
+
+sumar = suma(5,3)
+print("La suma es:", sumar)
+
+restar= resta(9,3)
+print("La resta es:",restar)
+
+multi = multiplica(4,9)
+print("La multiplicacion es:",multi)
+
+dividir = divide(8,2) 
+print("La division es:",dividir)
